e23dec7c4ffb5185ee47f062e9fc5eb78caf9026
SachinDevelopment <53714757+SachinDevelopment@users.noreply.github.com>
2022-06-11 22:08:04 -0400

    Update server.js

--- server.js
+++ server.js
@@ -714,7 +714,9 @@ io.on("connection", async (socket) => {
   socket.emit("playerOnline",  Array.from(onlinePlayers.values()));
 
   socket.on("randomize", async (selected) => {
-    io.emit("randomized", await randomize(selected));
+    const random = await randomize(selected)
+    console.log('random', random)
+    io.emit("randomized", random);
   });
 
   socket.on("redUpdate", async (red) => {
